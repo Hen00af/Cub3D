b2c03efc9285b80631a6d9523e67ea06a33eef5f
Naruki Ando <nando@c4r2s13.42tokyo.jp>
2025-09-13 18:21:49 +0900

    separate-src-files

---
+++ src/rendering/init_game.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_game.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nando <nando@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/13 16:02:04 by nando             #+#    #+#             */
+/*   Updated: 2025/09/13 18:19:34 by nando            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "../lib/cub3D.h"
+#include "./game.h"
+#include "./minilibx-Linux/mlx.h"
+
+# define WIN_W 960
+# define WIN_H 720
+
+void	init_game(t_game_data *game)
+{
+	game->mlx = mlx_init();
+	game->win = mlx_new_window(game->mlx, WIN_W, WIN_H, "Cub3D");
+	game->img.img = mlx_new_image(game->mlx, WIN_W, WIN_H);
+	game->img.address = mlx_get_data_addr();
+	mlx_loop(game->mlx);
+}
